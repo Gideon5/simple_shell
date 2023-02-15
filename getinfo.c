01816e943ca80f29c9299d68c6e5e96bd63804fe
Gideon Mba <abaaneg@gmail.com>
2023-02-14 18:08:36 -0800

    getinfo.c

---
+++ getinfo.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;	
